58f4d941b50147950af5ab55c62bb7c20967b465
siwakorn <siwakorn.m.0595@gmailcom>
2020-10-01 21:23:18 +0700

    template api constant

---
+++ app/src/main/java/com/siwakorn/mvvmtemplate/constant/ApiConstant.kt
@@ -0,0 +1,14 @@
+package com.appsynthassignment.constant
+
+object ApiConstant {
+
+    object EndPoint {
+        const val BASE_URL= "https://{BASE URL}"
+        const val API = "{api path}"
+    }
+
+    object Value {
+        const val VALUE = "{PATH VALUE}"
+    }
+
+}
