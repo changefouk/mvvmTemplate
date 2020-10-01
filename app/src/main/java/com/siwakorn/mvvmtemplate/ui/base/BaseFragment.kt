c3cec188ac25e7328a29acecb7274d6865c149dd
siwakorn <siwakorn.m.0595@gmailcom>
2020-10-01 21:49:28 +0700

    template Fragment data binding

---
+++ app/src/main/java/com/siwakorn/mvvmtemplate/ui/base/BaseFragment.kt
@@ -0,0 +1,29 @@
+package com.siwakorn.mvvmtemplate.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<Binding : ViewDataBinding> : Fragment() {
+
+    protected lateinit var binding: Binding
+
+    abstract fun getLayout(): Int
+
+    abstract fun onViewModelBinding()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, getLayout(), container, false) as Binding
+        binding.lifecycleOwner = this
+        onViewModelBinding()
+        return binding.root
+    }
+}
--- app/src/main/java/com/siwakorn/mvvmtemplate/ui/main/MainFragment.kt
+++ app/src/main/java/com/siwakorn/mvvmtemplate/ui/main/MainFragment.kt
@@ -1,30 +1,22 @@
 package com.siwakorn.mvvmtemplate.ui.main
 
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.siwakorn.mvvmtemplate.R
+import com.siwakorn.mvvmtemplate.databinding.MainFragmentBinding
+import com.siwakorn.mvvmtemplate.ui.base.BaseFragment
+import org.koin.android.ext.android.inject
 
-class MainFragment : Fragment() {
+class MainFragment : BaseFragment<MainFragmentBinding>() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by inject()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
+    override fun getLayout(): Int = R.layout.main_fragment
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewModelBinding() {
+        binding.vm = this.viewModel
     }
 
 }
